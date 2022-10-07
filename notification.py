f856180472faf9d6e13eb81bee9604eecd9defaa
prajnamort <prajnamort@gmail.com>
2022-10-07 02:08:15 -0400

    Re-organize code.

---
+++ notification.py
@@ -0,0 +1,40 @@
+import requests
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
+
+from .consts import USERNAME, SENDGRID_API_KEY, PUSH_TOKEN, PUSH_USER
+
+
+def send_notification(msg):
+    print(f"Sending notification: {msg}")
+
+    if SENDGRID_API_KEY:
+        message = Mail(
+            from_email=USERNAME,
+            to_emails=USERNAME,
+            subject=msg,
+            html_content=msg)
+        try:
+            sg = SendGridAPIClient(SENDGRID_API_KEY)
+            response = sg.send(message)
+            print(response.status_code)
+            print(response.body)
+            print(response.headers)
+        except Exception as e:
+            print(e.message)
+
+    if PUSH_TOKEN:
+        url = "https://api.pushover.net/1/messages.json"
+        data = {
+            "token": PUSH_TOKEN,
+            "user": PUSH_USER,
+            "message": msg
+        }
+        requests.post(url, data)
+
+
+def push_notification(dates):
+    msg = "date: "
+    for d in dates:
+        msg = msg + d.get('date') + '; '
+    send_notification(msg)
--- visa.py
+++ visa.py
@@ -2,193 +2,118 @@
 
 import time
 import json
-import random
-import platform
-import configparser
 from datetime import datetime
 
 import requests
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import WebDriverWait as Wait
-from selenium.webdriver.common.by import By
-from webdriver_manager.chrome import ChromeDriverManager
+from bs4 import BeautifulSoup
 
-from sendgrid import SendGridAPIClient
-from sendgrid.helpers.mail import Mail
-
-
-config = configparser.ConfigParser()
-config.read('config.ini')
-
-USERNAME = config['USVISA']['USERNAME']
-PASSWORD = config['USVISA']['PASSWORD']
-SCHEDULE_ID = config['USVISA']['SCHEDULE_ID']
-MY_SCHEDULE_DATE = config['USVISA']['MY_SCHEDULE_DATE']
-COUNTRY_CODE = config['USVISA']['COUNTRY_CODE'] 
-FACILITY_ID = config['USVISA']['FACILITY_ID']
-
-SENDGRID_API_KEY = config['SENDGRID']['SENDGRID_API_KEY']
-PUSH_TOKEN = config['PUSHOVER']['PUSH_TOKEN']
-PUSH_USER = config['PUSHOVER']['PUSH_USER']
-
-LOCAL_USE = config['CHROMEDRIVER'].getboolean('LOCAL_USE')
-HUB_ADDRESS = config['CHROMEDRIVER']['HUB_ADDRESS']
-
-REGEX_CONTINUE = "//a[contains(text(),'Continuar')]"
+from notification import send_notification, push_notification
 
 
 # def MY_CONDITION(month, day): return int(month) == 11 and int(day) >= 5
-def MY_CONDITION(month, day): return True # No custom condition wanted for the new scheduled date
-
-STEP_TIME = 0.5  # time between steps (interactions with forms): 0.5 seconds
-RETRY_TIME = 60*10  # wait time between retries/checks for available dates: 10 minutes
-EXCEPTION_TIME = 60*30  # wait time when an exception occurs: 30 minutes
-COOLDOWN_TIME = 60*60  # wait time when temporary banned (empty list): 60 minutes
-
-DATE_URL = f"https://ais.usvisa-info.com/{COUNTRY_CODE}/niv/schedule/{SCHEDULE_ID}/appointment/days/{FACILITY_ID}.json?appointments[expedite]=false"
-TIME_URL = f"https://ais.usvisa-info.com/{COUNTRY_CODE}/niv/schedule/{SCHEDULE_ID}/appointment/times/{FACILITY_ID}.json?date=%s&appointments[expedite]=false"
-APPOINTMENT_URL = f"https://ais.usvisa-info.com/{COUNTRY_CODE}/niv/schedule/{SCHEDULE_ID}/appointment"
-EXIT = False
-
-
-def send_notification(msg):
-    print(f"Sending notification: {msg}")
-
-    if SENDGRID_API_KEY:
-        message = Mail(
-            from_email=USERNAME,
-            to_emails=USERNAME,
-            subject=msg,
-            html_content=msg)
-        try:
-            sg = SendGridAPIClient(SENDGRID_API_KEY)
-            response = sg.send(message)
-            print(response.status_code)
-            print(response.body)
-            print(response.headers)
-        except Exception as e:
-            print(e.message)
-
-    if PUSH_TOKEN:
-        url = "https://api.pushover.net/1/messages.json"
-        data = {
-            "token": PUSH_TOKEN,
-            "user": PUSH_USER,
-            "message": msg
-        }
-        requests.post(url, data)
-
-
-def get_driver():
-    if LOCAL_USE:
-        dr = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-    else:
-        dr = webdriver.Remote(command_executor=HUB_ADDRESS, options=webdriver.ChromeOptions())
-    return dr
-
-driver = get_driver()
-
-
-def login():
-    # Bypass reCAPTCHA
-    driver.get(f"https://ais.usvisa-info.com/{COUNTRY_CODE}/niv")
-    time.sleep(STEP_TIME)
-    a = driver.find_element(By.XPATH, '//a[@class="down-arrow bounce"]')
-    a.click()
-    time.sleep(STEP_TIME)
-
-    print("Login start...")
-    href = driver.find_element(By.XPATH, '//*[@id="header"]/nav/div[2]/div[1]/ul/li[3]/a')
-    href.click()
-    time.sleep(STEP_TIME)
-    Wait(driver, 60).until(EC.presence_of_element_located((By.NAME, "commit")))
-
-    print("\tclick bounce")
-    a = driver.find_element(By.XPATH, '//a[@class="down-arrow bounce"]')
-    a.click()
-    time.sleep(STEP_TIME)
-
-    do_login_action()
-
-
-def do_login_action():
-    print("\tinput email")
-    user = driver.find_element(By.ID, 'user_email')
-    user.send_keys(USERNAME)
-    time.sleep(random.randint(1, 3))
-
-    print("\tinput pwd")
-    pw = driver.find_element(By.ID, 'user_password')
-    pw.send_keys(PASSWORD)
-    time.sleep(random.randint(1, 3))
-
-    print("\tclick privacy")
-    box = driver.find_element(By.CLASS_NAME, 'icheckbox')
-    box .click()
-    time.sleep(random.randint(1, 3))
-
-    print("\tcommit")
-    btn = driver.find_element(By.NAME, 'commit')
-    btn.click()
-    time.sleep(random.randint(1, 3))
-
-    Wait(driver, 60).until(
-        EC.presence_of_element_located((By.XPATH, REGEX_CONTINUE)))
-    print("\tlogin successful!")
+def MY_CONDITION(month, day): 
+    return True # No custom condition wanted for the new scheduled date
 
 
 def get_date():
-    driver.get(DATE_URL)
-    if not is_logged_in():
-        login()
-        return get_date()
+    global last_yatry_cookie
+
+    if last_yatry_cookie == "":
+        driver.get(DATE_URL)
+        if not is_logged_in():
+            login()
+            return get_date()
+        else:
+            content = driver.find_element(By.TAG_NAME, 'pre').text
+            date = json.loads(content)
+            last_yatry_cookie = "_yatri_session=" + driver.get_cookie("_yatri_session")["value"]
+            return date
     else:
-        content = driver.find_element(By.TAG_NAME, 'pre').text
-        date = json.loads(content)
-        return date
+        headers = base_headers
+        headers["Cookie"] = last_yatry_cookie
+        r = requests.get(DATE_URL, headers=headers)
+        print(r.text)
+        last_yatry_cookie = r.headers['Set-Cookie'].split(";")[0]
+        print(last_yatry_cookie)
+        return r.json()
 
 
 def get_time(date):
+    global last_yatry_cookie
+
+    print(f" Retrieving latest date for {date}")
     time_url = TIME_URL % date
-    driver.get(time_url)
-    content = driver.find_element(By.TAG_NAME, 'pre').text
-    data = json.loads(content)
+    headers = base_headers
+    headers["Cookie"] = last_yatry_cookie
+    r = requests.get(time_url, headers=headers)
+    data = json.loads(r.text)
     time = data.get("available_times")[-1]
-    print(f"Got time successfully! {date} {time}")
+    print(f"latest available time for {date}: {time}")
+    last_yatry_cookie = r.headers['Set-Cookie'].split(";")[0]
+    print(last_yatry_cookie)
     return time
+    # time_url = TIME_URL % date
+    # driver.get(time_url)
+    # content = driver.find_element(By.TAG_NAME, 'pre').text
+    # data = json.loads(content)
+    # time = data.get("available_times")[-1]
+    # print(f"Got time successfully! {date} {time}")
+    # return time
+
+def get_authenticity_token():
+    global last_yatry_cookie
+
+    headers = base_headers
+    headers["Referer"] = "https://ais.usvisa-info.com/en-ca/niv/schedule/42238236/continue_actions"
+    headers["Cookie"] = last_yatry_cookie
+    r = requests.get(APPOINTMENT_URL, headers=headers)
+    soup = BeautifulSoup(r.text, 'html.parser')
+    utf8 = soup.find('input', {'name': 'utf8'})['value']
+    token = soup.find('input', {'name': 'authenticity_token'})['value']
+    print(token)
+    last_yatry_cookie = r.headers['Set-Cookie'].split(";")[0]
+    print(last_yatry_cookie)
+    return utf8, token
 
 
 def reschedule(date):
-    global EXIT
+    global last_yatry_cookie, EXIT
     print(f"Starting Reschedule ({date})")
 
     time = get_time(date)
-    driver.get(APPOINTMENT_URL)
+    # time = "09:00"
+    utf8, token = get_authenticity_token()
 
     data = {
-        "utf8": driver.find_element(by=By.NAME, value='utf8').get_attribute('value'),
-        "authenticity_token": driver.find_element(by=By.NAME, value='authenticity_token').get_attribute('value'),
-        "confirmed_limit_message": driver.find_element(by=By.NAME, value='confirmed_limit_message').get_attribute('value'),
-        "use_consulate_appointment_capacity": driver.find_element(by=By.NAME, value='use_consulate_appointment_capacity').get_attribute('value'),
+        "utf8": utf8,
+        "authenticity_token": token,
+        "confirmed_limit_message": 1,
+        "use_consulate_appointment_capacity": "true",
         "appointments[consulate_appointment][facility_id]": FACILITY_ID,
         "appointments[consulate_appointment][date]": date,
         "appointments[consulate_appointment][time]": time,
     }
 
-    headers = {
-        "User-Agent": driver.execute_script("return navigator.userAgent;"),
-        "Referer": APPOINTMENT_URL,
-        "Cookie": "_yatri_session=" + driver.get_cookie("_yatri_session")["value"]
-    }
+    headers = base_headers
+    headers["Referer"] = APPOINTMENT_URL
+    headers["Cookie"] = last_yatry_cookie
+    # headers = {
+    #     "User-Agent": driver.execute_script("return navigator.userAgent;"),
+    #     "Referer": APPOINTMENT_URL,
+    #     "Cookie": "_yatri_session=" + driver.get_cookie("_yatri_session")["value"]
+    # }
 
     r = requests.post(APPOINTMENT_URL, headers=headers, data=data)
-    if(r.text.find('Successfully Scheduled') != -1):
+    last_yatry_cookie = r.headers['Set-Cookie'].split(";")[0]
+    print(last_yatry_cookie)
+    if(r.text.find('successfully scheduled') != -1):
         msg = f"Rescheduled Successfully! {date} {time}"
         send_notification(msg)
-        EXIT = True
+        MY_SCHEDULE_DATE = date
+        print("updated schedule date to %s", date)
+        # EXIT = True
     else:
+        print(r.text)
         msg = f"Reschedule Failed. {date} {time}"
         send_notification(msg)
 
@@ -223,25 +148,20 @@ def get_available_date(dates):
     print("Checking for an earlier date:")
     for d in dates:
         date = d.get('date')
-        if is_earlier(date) and date != last_seen:
-            _, month, day = date.split('-')
-            if(MY_CONDITION(month, day)):
-                last_seen = date
-                return date
-
-
-def push_notification(dates):
-    msg = "date: "
-    for d in dates:
-        msg = msg + d.get('date') + '; '
-    send_notification(msg)
+        if is_earlier(date):
+            return date
+        # if is_earlier(date) and date != last_seen:
+        #     _, month, day = date.split('-')
+            # if(MY_CONDITION(month, day)):
+            #     last_seen = date
+            #     return date
 
 
 if __name__ == "__main__":
-    login()
+    # login()
     retry_count = 0
     while 1:
-        if retry_count > 6:
+        if retry_count > 100:
             break
         try:
             print("------------------")
@@ -253,7 +173,7 @@ if __name__ == "__main__":
             if not dates:
               msg = "List is empty"
               send_notification(msg)
-              EXIT = True
+              EXIT = False
             print_dates(dates)
             date = get_available_date(dates)
             print()
